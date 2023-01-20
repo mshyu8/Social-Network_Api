ecd654bc87e4e3e5561b74841ad1c1c5a2f3719c
Michael Shyu <michaelshyu11@gmail.com>
2023-01-20 16:51:08 -0500

    thought route

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  removeThought,
+  createReaction,
+  removeReaction,
+} = require('../../controllers/thoughtController.js');
+
+router.route('/').get(getThoughts).post(createThought);
+
+router
+  .route('/:thoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(removeThought);
+
+router
+  .route('/:thoughtId/reactions')
+  .post(createReaction)
+  .delete(removeReaction);
+
+module.exports = router;
