b73fb05b1150d30185be6e8f5572d38f2359ce18
Michael Shyu <michaelshyu11@gmail.com>
2023-01-20 02:19:19 -0500

    finished user model

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model, Types } = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            trim: true,
+            required: true,
+            max_length: 50,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate:{
+                validator: validator.isEmail,
+                message: `Not a valid email address`,
+                isAsync: false
+            }
+        },
+        thoughts: [
+            {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true,
+        },
+        id: false,
+    });
+
+    userSchema.virtual('friendCount').get(function() {
+        return this.friends.length;
+    });
+
+    module.exports = User;
