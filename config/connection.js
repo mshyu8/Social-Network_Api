f84b62313fcea6aef5732c9358e13c812c3c198c
Michael Shyu <michaelshyu11@gmail.com>
2023-01-20 02:08:58 -0500

    added connection.js

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+const {connect, connection} = require('mongoose');
+
+
+const connectionString = 'mongodb://127.0.0.1:27017/socialnetworkapiDB';
+
+connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+module.exports = connection;
+    
